33a22c67b234c471055b6d23c84dac39eedd2b7a
bayram bartu <bayram213062@ogr.duzce.edu.tr>
2024-10-01 00:49:58 +0300

    concurrency
    golang concurrency EP1

---
+++ Golang_Concurrency_EP1/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	start := time.Now()
+	defer func() {
+		fmt.Println(time.Since(start))
+	}()
+
+	evilNinjas := []string{"Tommy", "Johnny", "Bobby", "Andy"}
+	for _, evilNinja := range evilNinjas {
+		go attack(evilNinja)
+	}
+	time.Sleep(time.Second * 2)
+
+}
+
+func attack(target string) {
+	fmt.Println("Throwing ninja starts at", target)
+	time.Sleep(time.Second)
+}
